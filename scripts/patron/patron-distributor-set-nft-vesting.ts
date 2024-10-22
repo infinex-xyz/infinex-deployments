b70f5fdfc9ed7435f9d117e5c35accd98476b415
Ray <165126287+rayInfinex@users.noreply.github.com>
2024-10-22 23:38:00 +1100

    add error logging

--- scripts/patron/patron-distributor-set-nft-vesting.ts
+++ scripts/patron/patron-distributor-set-nft-vesting.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { createContext } from './common';
+import { formatEther } from 'viem';
 
 async function main() {
 
@@ -20,10 +21,24 @@ async function main() {
   const initialBalance = await publicClient.getBalance({
     address: account.address,
   });
-  console.log(`Initial ETH balance: ${initialBalance} wei`);
+  console.log(`Initial ETH balance: ${formatEther(initialBalance)} ETH`);
 
   const patronNft = PATRON_NFT_ADDRESS as `0x${string}`;
   const vestingContract = PATRON_VESTING_ADDRESS as `0x${string}`;
+
+  // Simulate setPatronNft transaction
+  try {
+    await publicClient.simulateContract({
+      account,
+      address: patronDistributor.address,
+      abi: patronDistributor.abi,
+      functionName: 'setPatronNft',
+      args: [patronNft],
+    });
+    console.log('Simulation of setPatronNft successful');
+  } catch (error) {
+    console.error('Error simulating setPatronNft:', error);
+  }
 
   const registerPatronNFTTx = await walletClient.writeContract({
     address: patronDistributor.address,
@@ -38,6 +53,20 @@ async function main() {
   );
   totalGasUsed += registerPatronNFTTxReceipt.gasUsed;
   console.log(`Total gas used: ${totalGasUsed}`);
+
+  // Simulate setPatronVesting transaction
+  try {
+    await publicClient.simulateContract({
+      account,
+      address: patronDistributor.address,
+      abi: patronDistributor.abi,
+      functionName: 'setPatronVesting',
+      args: [vestingContract],
+    });
+    console.log('Simulation of setPatronVesting successful');
+  } catch (error) {
+    console.error('Error simulating setPatronVesting:', error);
+  }
 
   const registerVestingContractTx = await walletClient.writeContract({
     address: patronDistributor.address,
